dbb060893c938dd2cd68ae9e155bff6999e1217e
Aabid Dewan <aabid@Aabids-MacBook-Air.local>
2025-02-18 19:58:43 -0500

    UpdatedCode

--- Animal.py
+++ Animal.py
@@ -1,7 +1,23 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, cry, color, species, age):
+        self.__name = name 
+        self.__cry = cry
+        self.__color = color
+        self.__species = species
+        self.__age = age
+        
+    def name(self):
+        print("This animal is a", self.__name) 
 
-    def talk(self):
-        print("hi")
+    def cry(self):
+        print("It makes the sound", self.__cry)
+
+    def color(self):
+        print("The color of the animal is", self.__color) 
+
+    def species(self):
+        print("The animal's species is", self.__species)
+
+    def age(self):
+        print("The animal is", self.__age, "years old")
 
